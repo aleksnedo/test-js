ba9cee0206074f25de02b99f996419a0e3f031c4
Oleksii Nedohybchenko <104201289+aleksnedo@users.noreply.github.com>
2022-11-16 18:07:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -105,3 +105,75 @@ let call = randomPlace1 + ' vs ' + randomPlace2;
 console.log(call);
 
 // --------
+
+const footballPlayer = {
+  name: 'Luka',
+  surname: 'Modric',
+  team: 'Real Madrid',
+};
+
+console.log(Object.keys(footballPlayer));
+
+let footballPlayer2 = {};
+footballPlayer2.name = 'Robert';
+footballPlayer2.surname = 'Lewandowski';
+footballPlayer2.team = 'Barcelona';
+
+console.log(footballPlayer2);
+
+console.log(Object.values(footballPlayer2));
+
+// -----------
+
+const firstPlayer = {
+  name: 'Cristiano',
+  surname: 'Ronaldo',
+  clubs: ['Sporting', 'ManUtd', 'RealMadrid', 'Juventus'],
+};
+const secondPlayer = {
+  name: 'Eden',
+  surname: 'Hazard',
+  clubs: ['Lille', 'Chelsea', 'RealMadrid'],
+};
+
+const twoPlayers = [firstPlayer, secondPlayer];
+
+console.log(twoPlayers[1]);
+console.log(twoPlayers[0].clubs[3]);
+
+// --------
+
+const movies = {
+  'Finding Nemo': {
+    releaseDate: 2003,
+    duration: 100,
+    actors: ['Albert Brooks', 'Ellen DeGeneres', 'Alexander Gould'],
+    format: 'DVD',
+  },
+  'Star Wars: Episode VI - Return of the Jedi': {
+    releaseDate: 1983,
+    duration: 134,
+    actors: ['Mark Hamill', 'Harrison Ford', 'Carrie Fisher'],
+    format: 'DVD',
+  },
+  'Harry Potter and the Goblet of Fire': {
+    releaseDate: 2005,
+    duration: 157,
+    actors: ['Daniel Radcliffe', 'Emma Watson', 'Rupert Grint'],
+    format: 'Blu-ray',
+  },
+};
+
+console.log(movies['Finding Nemo']);
+console.log(Object.keys(movies));
+
+// ---------
+const myCrazyObject = {
+  name: 'A ridiculous object',
+  'some array': [7, 9, { purpose: 'confusion', number: 123 }, 3.3],
+  'random animal': 'Banana Shark',
+};
+
+console.log(myCrazyObject['some array'][2].number);
+
+// -----
