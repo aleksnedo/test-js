fd47a128fae1819b8fb3db72d4d8f41dd8611a33
aleksnedo <104201289+aleksnedo@users.noreply.github.com>
2023-02-12 20:52:14 +0000

    Deploying to gh-pages from @ aleksnedo/test-js@c92288bacc2aa1df3ea64555e49746417b0ff577 🚀

---
+++ partials/async.9938bd9b.js
@@ -0,0 +1,2 @@
+function o(o){return new Promise(((n,t)=>{o<500&&t`Слишком мало сна!`,setTimeout((()=>n(`Поспал ${o}`)),o)}))}(async()=>{try{const n=await o(2e3);console.log(n);const t=await o(1500);console.log(t);const c=await o(1e3);console.log(c);const e=await o(400);console.log(e)}catch(o){console.log(o)}})();
+//# sourceMappingURL=async.9938bd9b.js.map
